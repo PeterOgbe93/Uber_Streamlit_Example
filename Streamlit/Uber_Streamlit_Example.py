05acf5ea2757c9da435aaa0546d3ed48dc152ac9
PeterOgbe93 <sba25279@student.cct.ie>
2026-05-12 19:20:27 +0100

    c

--- Streamlit/Uber_Streamlit_Example.py
+++ Streamlit/Uber_Streamlit_Example.py
@@ -19,15 +19,16 @@ data_load_state=st.text('Loading Data...')
 data=load_data(10000)
 data_load_state.text('Loading Data Done')
 
-st.subheader('Raw Data')
-st.write(data)
+if st.checkbox('show raw data '):
+	st.subheader('Raw Data')
+	st.write(data)
 
 st.subheader("Num Pickups per hour")
-hist_values=np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24)),[0]
+hist_values=np.histogram(data[DATE_COLUMN].dt.hour,bins=24,range=(0,24))[0]
 st.bar_chart(hist_values)
 
 st.subheader('Maps of Pickups')
-hist_values=np.histogram(data[DATA_COLUMNS].dt.hour, bins=24, range+(0,24)),[0]
+hist_values=np.histogram(data[DATA_COLUMNS].dt.hour,bins=24,range+(0,24))[0]
 st.bar_chart(host_values)
 
 hour_to_filter=st.slider('hour', 0,23,17)
